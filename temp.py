d76ebd41621825ed6fef725e18b6e75c9e536625
Edmund <eyu1@ualberta.ca>
2023-10-16 13:29:21 -0600

    add fahrenheit to celsius function

--- temp.py
+++ temp.py
@@ -1,8 +1,14 @@
 # (Celsius × 9/5) + 32 = 32°F
+# (Fahrenheit − 32) × 5/9 = 0°C
 
 
 def celsius_to_fahrenheit(celsius):
     return celsius * (9 / 5) + 32
 
 
+def fahrenheit_to_celsius(fahrenheit):
+    return (fahrenheit - 32) * (5 / 9)
+
+
 print(f"25 celsius is equal to {celsius_to_fahrenheit(25):.0f} fahrenheit")
+print(f"82 fahrenheit is equal to {fahrenheit_to_celsius(82):.0f} degree celsius.")
