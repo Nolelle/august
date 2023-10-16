574a2c72f224ef55ccdab8b42ffa4be092b5d8fe
Edmund <eyu1@ualberta.ca>
2023-10-16 13:26:50 -0600

    update temp.py with a celsius to fahrenheit function

--- temp.py
+++ temp.py
@@ -1,6 +1,8 @@
 # (Celsius × 9/5) + 32 = 32°F
 
-celsius = 25
-fahrenheit = celsius * (9 / 5) + 32
 
-print(f"25 celsius is equal to {fahrenheit:.0f} fahrenheit")
+def celsius_to_fahrenheit(celsius):
+    return celsius * (9 / 5) + 32
+
+
+print(f"25 celsius is equal to {celsius_to_fahrenheit(25):.0f} fahrenheit")
